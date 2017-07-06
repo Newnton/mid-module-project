563907c691563259f2a9d0fda8599c8aa16fb3da
Zach Dubno <zachedubno@gmail.com>
2017-07-06 13:02:09 -0400

    fixed expand button for posts with URLs in the main body

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function displayStuff(data){
   data.forEach((shot)=>{
     // let title = shot.title
     if(shot.description && shot.description.length > 240){
-      $('#main-container .card-columns').append(`<div class="card" style="width: 410px; border: 1px solid #0275d8;"><img class="card-img-top" src="${shot.images.normal}"><div class="card-block"><h4 class="card-title">${shot.title}</h4><p class="card-text" id='${shot.id}'>${shot.description.substring(0, 240)}...</p><br><div class="card-footer"><div style="class="btn-group" role="group"><button type="button" id='more-${shot.id}' class="btn btn-outline-success">...</button><a href=${shot.html_url} class="btn btn-outline-primary">view post</a></div></div></div></div>`)
+      $('#main-container .card-columns').append(`<div class="card" style="width: 410px; border: 1px solid #0275d8;"><img class="card-img-top" src="${shot.images.normal}"><div class="card-block"><h4 class="card-title">${shot.title}</h4><p class="card-text" id='${shot.id}'>${shot.description.substring(0, 240)}...</p><br><div class="card-footer"><a href='/' class="btn btn-outline-success" id='more-${shot.id}' >...</button><a href=${shot.html_url} class="btn btn-outline-primary">view post</a></div></div></div>`)
       addMoreListener(shot.id)
     }
     else{
--- onClick.js
+++ onClick.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 });
 
 function addMoreListener(id){
-  $(`#more-${id}`).on('click', function(){
-    console.log(`clicked ${id}`)
+  $(`#more-${id}`).on('click', function(event){
+    event.preventDefault()
     $.get(`https://api.dribbble.com/v1/shots/${id}?access_token=10e05172474bedfe6a6102e4d6e08be4bf6f82f29cc8b7094db994c55efe85b3`, function(data){
       $(`#${id}`).html(data.description)
     })
